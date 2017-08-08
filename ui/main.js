2eb31f11e2f20ab61540025a43fbd152f8c83bc5
Harshitjindal1999 <30645592+Harshitjindal1999@users.noreply.github.com>
2017-08-08 22:05:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,6 +22,6 @@ button.onclick = function () {
     
   //make the request
   
-     request.open('GET' , 'http://jindalharshit14.imad.hasura-app.io/counter',  true);
+     request.open('GET' ,'http://jindalharshit14.imad.hasura-app.io/counter', true);
      request.send(null);
 };
