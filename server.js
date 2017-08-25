4ee1107839025c5a5297f5f049f380b44f049cbc
Harshitjindal1999 <30645592+Harshitjindal1999@users.noreply.github.com>
2017-08-25 13:04:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
- /* var pool = new pool(config);
+  var pool = new pool(config);
 app.get('/test-db', function (req, res) {
  pool.query('SELECT * FROM test', function (err, result){
      if (err) {
@@ -113,7 +113,7 @@ app.get('/test-db', function (req, res) {
           res.send(JSON.stringify(result));
      }
  });
-}); */
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
